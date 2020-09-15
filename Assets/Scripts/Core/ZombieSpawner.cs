162de49e80f24e2ebbda26a2f5b8a4c47d00cfef
sakloui <42945324+sakloui@users.noreply.github.com>
2020-09-15 13:50:17 +0200

    Added more zombie stuff

---
+++ Assets/Scripts/Core/ZombieSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+
+    public GameObject PrefabObject;
+    public int SpawnCount;
+    public GameObject[] SpawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Awake()
+    {
+        for (int i = 0; i < SpawnCount; i++)
+        {
+            GameObject spawnpoint = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
+            GameObject zombie = Instantiate(PrefabObject, spawnpoint.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
