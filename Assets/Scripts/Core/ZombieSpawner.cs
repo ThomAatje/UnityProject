0951665b7e3345372fa2ecda39a3bee5aa3493d0
sakloui <42945324+sakloui@users.noreply.github.com>
2020-09-22 13:21:21 +0200

    Zombie edit

---
+++ Assets/Scripts/Core/ZombieSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    public int MAX_ZOMBIE_COUNT = 15;
+    public float SpawnDelay = 1;
+
+    public GameObject PrefabObject;
+    public GameObject[] SpawnPoints;
+
+    private float CurrentSpawnTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnUpdate();
+        ZombieUpdate();
+    }
+
+    // Game updates
+    void SpawnUpdate()
+    {
+        CurrentSpawnTime += Time.deltaTime;
+    }
+
+    void ZombieUpdate()
+    {
+
+    }
+}
